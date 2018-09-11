1ea2772f9d40dc64cc399499046915128985f36c
Stella Cannefax <stella@unity3d.com>
2018-09-10 18:38:01 -0700

    kinda works now ??? needs visual output

--- Assets/GpuReadbackJobExample.cs
+++ Assets/GpuReadbackJobExample.cs
@@ -4,14 +4,14 @@ using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class WebcamLuminance : MonoBehaviour
+public class GpuReadbackJobExample : MonoBehaviour
 {
 	[SerializeField]
 	RenderTexture m_FlatTexture;
 	
 	WebCamTexture m_CamTexture;
 
-	ComputeBuffer m_Buffer;
+	[SerializeField] Renderer m_SecondRenderer;
 
 	AsyncGPUReadbackRequest m_ReadbackRequest;
 
@@ -19,14 +19,18 @@ public class WebcamLuminance : MonoBehaviour
 	
 	NativeArray<Color32> m_ReadbackData;
 
+	Texture2D m_LoadBuffer;
+
 	
 	void Start ()
 	{
+		m_LoadBuffer = new Texture2D(1280, 720);
 		m_FlatTexture = new RenderTexture(1280, 720, 1);
 		m_FlatTexture.enableRandomWrite = true;
 		
 		m_CamTexture = new WebCamTexture(1280, 720);
 		GetComponent<Renderer>().material.mainTexture = m_FlatTexture;
+		m_SecondRenderer.material.mainTexture = m_LoadBuffer;
 		m_CamTexture.Play();
 		
 		Graphics.Blit(m_CamTexture, m_FlatTexture);
@@ -37,29 +41,22 @@ public class WebcamLuminance : MonoBehaviour
 		});
 	}
 
-
-	void OnDestroy()
-	{
-		if(m_Buffer != null)
-			m_Buffer.Release();
-	}
-
 	void Update ()
 	{
 		Graphics.Blit(m_CamTexture, m_FlatTexture);
 		if (!m_PostReadbackJobHandle.IsCompleted)
 		{
 			m_PostReadbackJobHandle.Complete();
-
+			//m_LoadBuffer.LoadRawTextureData(m_ReadbackData);
+			//m_LoadBuffer.Apply();
 		}
-
+		
 		if (!m_ReadbackRequest.done)
 			return;
 
 		if (m_ReadbackRequest.hasError)
 		{
 			Debug.LogError("error during readback ??");
-			return;
 		}
 
 		m_ReadbackRequest = AsyncGPUReadback.Request(m_FlatTexture, 0, request =>
